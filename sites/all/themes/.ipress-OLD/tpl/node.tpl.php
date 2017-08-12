347401943f286564f2d39d72044fc7b926a4c95f
Andy Alexander <andy@balloonindustries.com>
2017-08-12 13:04:03 -0400

    test

---
+++ sites/all/themes/.ipress-OLD/tpl/node.tpl.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a node.
+ */
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h1<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php //print $submitted; 
+	    $node_author = ($node->name);               
+	    if ($submitted) { 
+            echo "By " . $node_author . "   " . date( "F j, Y",$node->created);  
+   }  
+        ?>
+    </div>
+  <?php endif; ?>
+
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+
+  <?php print render($content['links']); ?>
+
+  <?php //print render($content['comments']); ?>
+
+</div>
---
+++ sites/all/themes/ipress/tpl/header.tpl.php
@@ -0,0 +1,106 @@
+<header id="header">
+	<div class="a_head">
+		<div class="row clearfix">
+			
+			<?php
+				if ($page['left_bar']):
+			?>
+			<div class="breaking_news lefter">
+				<div class="freq_out">
+					<div class="freq">
+						<div class="inner_f"></div>
+						<div id="layerBall"></div>
+					</div>
+				</div>
+				<?php
+					print render($page['left_bar']);
+				?>
+			</div>
+			<?php
+				endif;
+			?>
+			<!-- /breaking news -->
+			<div class="right_bar">
+				<?php
+					if ($page['right_bar']):
+						print render($page['right_bar']);
+					endif;
+				?>
+				<!-- /social -->
+				<span id="date_time"></span>
+				<!-- /date -->
+			</div>
+			<!-- /right bar -->
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /a head -->
+	<div class="b_head">
+		<div class="row clearfix">
+			
+                        <!-- /old ads spot -->
+			
+			<!-- /old ads -->
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /b head -->
+	<div class="row clearfix">
+		<div class="sticky_true">
+			<div class="c_head clearfix">
+				<nav>
+				<div class="logo">
+				<?php
+					if($logo){
+				?>
+				<a href="<?php print check_url($front_page); ?>" title="<?php print $site_name; ?>"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" /></a>
+				<?php }?>
+				<?php if ($site_slogan){ ?>
+				<div id="site-slogan"><?php print $site_slogan; ?></div>
+				<?php } ?>
+			</div>
+			<!-- /logo -->
+					<?php 
+						if ($page['main_menu']): 
+							//$menu_name = variable_get('menu_main_links_source', 'main-menu'); 
+							//$tree = menu_tree($menu_name); 
+							print render($page['main_menu']);
+							//print drupal_render($tree, array('links' => $main_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'sf-menu sf-js-enabled sf-shadow')))); 
+						endif;
+					?>
+					<div class="right_icons">
+					<div class="search">
+						<!--<div class="search_icon"><i class="fa-search"></i></div>-->
+					<?php
+								if ($page['search_form']):
+									print render($page['search_form']);
+								endif;
+							?>
+						</div>
+					</div>
+				<!-- /right icons -->
+				</nav>
+				<!-- /nav -->
+				
+					<!-- /random post -->
+<!--					
+
+						<!-- /s form --> 
+										<!-- /search -->
+				
+			</div>
+			<!-- /c head -->
+		</div>
+		<!-- /sticky -->
+                        <?php
+				if ($page['ads_banner_right']):
+					print '<div class="ads">';
+						print render($page['ads_banner_right']);
+					print '</div>';
+				endif;
+			?>
+			
+			<!-- /ads -->
+	</div>
+	<!-- /row -->
+</header>
